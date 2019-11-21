d910c5478b3348393fd962b171dd353910c80d88
Wellici Araujo <wja1@ifal.edu.br>
2019-11-21 20:13:33 -0300

    created routes delete and put

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
+import ToolsController from "./app/controllers/ToolsController";
+
 const routes = Router();
 
-routes.get("/test", (req, res) => {
-  res.json({ ok: true });
-});
+routes.get("/tools", ToolsController.index);
+routes.post("/tools", ToolsController.store);
+routes.put("/tools/:id", ToolsController.update);
+routes.delete("/tools/:id", ToolsController.delete);
 
 export default routes;
