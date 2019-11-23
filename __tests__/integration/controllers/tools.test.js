0f42daf2ec333b78371ee0bedb6ded3685d793d6
Wellici Araujo <wja1@ifal.edu.br>
2019-11-23 18:00:35 -0300

    added data fake in tests of tools

--- __tests__/integration/controllers/tools.test.js
+++ __tests__/integration/controllers/tools.test.js
@@ -1,18 +1,26 @@
 /* eslint-disable no-undef */
 import request from "supertest";
+import mongoose from "mongoose";
+import faker from "faker";
 import app from "../../../src/app";
+import Tool from "../../../src/app/models/Tool";
 
 describe("Tools", () => {
   it("Should create a tool", async () => {
-    await request(app)
+    const data = {
+      title: faker.name.title(),
+      description: faker.name.jobDescriptor(),
+      tags: faker.random
+        .words(Math.floor(Math.random() * (6 - 1) + 1))
+        .split(" "),
+      link: faker.internet.url()
+    };
+    const tool = await request(app)
       .post("/tools")
-      .send({
-        title: "Test",
-        description: "fazendo testes",
-        tags: ["test 1", "test 2"],
-        link: "www.google.com"
-      })
+      .send(data)
       .expect(201);
+
+    expect(tool.body.message).toBe("Created");
   });
 
   it("Should return all tools of database", async () => {
@@ -21,5 +29,28 @@ describe("Tools", () => {
       .expect(200);
 
     expect(response.body.length).not.toBe(0);
+  });
+
+  it("Should delete that tool created", async () => {
+    const data = {
+      title: faker.name.title(),
+      description: faker.name.jobDescriptor(),
+      tags: faker.random
+        .words(Math.floor(Math.random() * (6 - 1) + 1))
+        .split(" "),
+      link: faker.internet.url()
+    };
+    const { _id } = await Tool.create(data);
+
+    await request(app)
+      .delete(`/tools/${_id}`)
+      .expect(204);
+  });
+
+  it("Should return status 404 when id tool not exist", async () => {
+    const id = mongoose.Types.ObjectId();
+    await request(app)
+      .delete(`/tools/${id}`)
+      .expect(404);
   });
 });
