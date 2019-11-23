0c8acd29dad2b790a3fe923c9cd9992056016586
Wellici Araujo <wja1@ifal.edu.br>
2019-11-23 12:56:31 -0300

    added properties in file configuration database

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,8 @@ export default mongoose.connect(
   `mongodb://localhost:27017/${process.env.DB_TEST || DB}`,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
 );
