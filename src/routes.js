dc27898378ab83734431f264d0470199639dd629
Wellici Araujo <wja1@ifal.edu.br>
2019-11-23 13:02:16 -0300

    new routes added in application, for user authentication and authorization

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 
+import auth from "./app/middlewares/auth";
+
 import ToolsController from "./app/controllers/ToolsController";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
 
 const routes = Router();
 
@@ -8,5 +12,12 @@ routes.get("/tools", ToolsController.index);
 routes.post("/tools", ToolsController.store);
 routes.put("/tools/:id", ToolsController.update);
 routes.delete("/tools/:id", ToolsController.delete);
+
+routes.post("/users", UserController.store);
+
+routes.get("/users/me", auth, SessionController.show);
+routes.post("/users/login", SessionController.store);
+routes.delete("/users/me/logout", auth, SessionController.delete);
+routes.delete("/users/me/logoutall", auth, SessionController.deleteAll);
 
 export default routes;
