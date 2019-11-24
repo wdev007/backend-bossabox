df257f19ed0d97c56f9b74460b3512314180e4f7
Wellici Araujo <wja1@ifal.edu.br>
2019-11-23 23:37:44 -0300

    routes of user added

--- src/routes.js
+++ src/routes.js
@@ -13,11 +13,16 @@ routes.post("/tools", ToolsController.store);
 routes.put("/tools/:id", ToolsController.update);
 routes.delete("/tools/:id", ToolsController.delete);
 
-routes.post("/users", UserController.store);
-
 routes.get("/users/me", auth, SessionController.show);
+routes.get("/users/log", auth, SessionController.show);
 routes.post("/users/login", SessionController.store);
 routes.delete("/users/me/logout", auth, SessionController.delete);
 routes.delete("/users/me/logoutall", auth, SessionController.deleteAll);
+
+routes.post("/users", UserController.store);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
+routes.get("/users/:id", UserController.show);
+routes.get("/users", UserController.index);
 
 export default routes;
